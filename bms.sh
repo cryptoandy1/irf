6c8404a498ba379acbf4d64b91e091c63652f53c
cryptoandy1 <98280054+cryptoandy1@users.noreply.github.com>
2023-01-28 13:54:01 +0300

    Update bms.sh

--- bms.sh
+++ bms.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+REQUIRED_PKG="bc"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
 
 function WaitTransactionToBeCompleted() {
     HASH=${1}
